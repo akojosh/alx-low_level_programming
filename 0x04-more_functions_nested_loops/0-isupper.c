5438831880a16f19356c1160976ca5da28434700
akojosh <akojosh1@gmail.com>
2022-10-27 23:12:33 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isuper - functions that checks for uppercase letter
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c);
+{
+	if ((c >= 'A') && (c <= 'z'))
+
+		return (1);
+	
+	else
+		return (0);
+}
